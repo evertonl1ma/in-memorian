9de7f214724fba54b70e5b4906ca58f814131be2
Everton Lima <ever-tonlima@hotmail.com>
2017-03-01 10:45:34 -0300

    Atualizando

--- public/js/controllers/InMemorianCtrl.js
+++ public/js/controllers/InMemorianCtrl.js
@@ -1,16 +1,3 @@
-angular.module("inMemorian").controller("inMemorianCtrl", function($scope, $http) {
-	$scope.scientists= [];
-	$scope.filtro= "";
-	
-	$scope.toTop= function() {
-		document.body.scrollTop= 0;
-	};
-
-	$http.get("https://in-memorian.herokuapp.com/scientists")
-	.then(function(success) { 
-		$scope.scientists= success.data;
-	 }, 
-	 function(error) {
-	 	console.log(error)
-	 });
+angular.module("inMemorian").controller("inMemorianCtrl", function($scope) {
+	$scope.titulo= "In inMemorian Teste";
 });
